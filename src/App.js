849f21f46031d6c4e00be08666fb83bf6440b51a
dilli babu <dillibabukadati777@gmail.com>
2022-02-09 08:01:50 +0530

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-[#282c34] min-h-screen flex flex-col align-middle justify-center text-white ">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
