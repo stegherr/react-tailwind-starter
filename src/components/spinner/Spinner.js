147153f136bdd23c5c8b9f05eff536f3aab7c6dd
dilli babu <dillibabukadati777@gmail.com>
2022-02-13 21:28:36 +0530

    added logo based spinner

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,7 +1,7 @@
 import ClipLoader from "react-spinners/HashLoader";
 import { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
-
+import classes from "./Spinner.module.css";
 const Spinner = (props) => {
   const isShowLoading = useSelector((state) => state.ui.isShowLoading);
 
@@ -10,8 +10,18 @@ const Spinner = (props) => {
   return (
     <Fragment>
       {isShowLoading && (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
+        <div
+          className={`fixed inset-0 bg-gray-800 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center pointer-events-none ${classes.overlay}`}
+        >
           <ClipLoader color={color} loading={true} size={100} />
+
+          {/* Enable the below for logo based loader */}
+          {/* <img
+            className={`${classes.foo} bg-slate-700`}
+            src={"logo512.png"}
+            height="200"
+            width="200"
+          /> */}
         </div>
       )}
     </Fragment>
